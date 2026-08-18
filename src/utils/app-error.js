827da201b106b559f2528f57101da47d0683c9fe
juansfer858 <146719055+juansfer858@users.noreply.github.com>
2026-08-18 16:43:45 -0500

    Add application error utility

---
+++ src/utils/app-error.js
@@ -0,0 +1,11 @@
+class AppError extends Error {
+  constructor(statusCode, message, code = 'APP_ERROR', details = undefined) {
+    super(message);
+    this.name = 'AppError';
+    this.statusCode = statusCode;
+    this.code = code;
+    this.details = details;
+  }
+}
+
+module.exports = { AppError };
