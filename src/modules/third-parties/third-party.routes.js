ee6840f6b344c806b11cfcc54910bfcfbf2e3f81
juansfer858 <146719055+juansfer858@users.noreply.github.com>
2026-08-18 20:03:46 -0500

    Super Core: add third party routes

---
+++ src/modules/third-parties/third-party.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const controller = require('./third-party.controller');
+
+const router = express.Router();
+
+router.get('/', controller.list);
+router.post('/', controller.create);
+router.get('/:id', controller.getById);
+router.patch('/:id', controller.update);
+
+module.exports = { thirdPartyRouter: router };
