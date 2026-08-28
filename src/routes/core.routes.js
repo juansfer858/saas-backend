472a24e9ae4c082aa6d8689580ee5895994f7059
juansfer858 <146719055+juansfer858@users.noreply.github.com>
2026-08-27 22:30:56 -0500

    feat(restaurant): mount secure visit and split payment staff API

--- src/routes/core.routes.js
+++ src/routes/core.routes.js
@@ -18,6 +18,7 @@ const { edgeTenantRouter } = require('../modules/edge/edge.routes');
 const { notificationsRouter } = require('../modules/notifications/notifications.routes');
 const { metaTechRouter } = require('../modules/notifications/meta-tech.routes');
 const { restaurantRouter } = require('../modules/restaurant/restaurant.routes');
+const { restaurantVisitPaymentsRouter } = require('../modules/restaurant/restaurant-visit-payments.routes');
 const { restaurantSelfServiceTenantRouter } = require('../modules/self-service/restaurant-self-service.routes');
 require('../modules/restaurant/restaurant-draft-fix');
 const { installRestaurantRbac } = require('../modules/restaurant/restaurant.rbac');
@@ -47,5 +48,6 @@ router.use('/edge', edgeTenantRouter);
 router.use('/notificaciones', metaTechRouter);
 router.use('/notificaciones', notificationsRouter);
 router.use('/restaurante', restaurantRouter);
+router.use('/restaurante', restaurantVisitPaymentsRouter);
 
 module.exports = { coreRouter: router };
