7383d2ab8b45dc8c0095d7397b267686a304f158
juansfer858 <146719055+juansfer858@users.noreply.github.com>
2026-09-10 12:45:30 -0500

    feat(restaurante): exponer asset de impresora híbrida V24

--- src/modules/restaurant/restaurant-v2-kds.public.routes.js
+++ src/modules/restaurant/restaurant-v2-kds.public.routes.js
@@ -10,6 +10,7 @@ function send(res,file,type){res.set('Cache-Control','no-store, max-age=0');res.
 router.get('/app/restaurante-v2/kds',(_req,res)=>send(res,'restaurant-v2-kds.html','html'));
 router.get('/app/restaurant-v2-kds.js',(_req,res)=>send(res,'restaurant-v2-kds.js','application/javascript; charset=utf-8'));
 router.get('/app/restaurant-v2-kds-stations-v23.js',(_req,res)=>send(res,'restaurant-v2-kds-stations-v23.js','application/javascript; charset=utf-8'));
+router.get('/app/restaurant-v2-kds-printer-hybrid-v24.js',(_req,res)=>send(res,'restaurant-v2-kds-printer-hybrid-v24.js','application/javascript; charset=utf-8'));
 router.get('/app/restaurant-v2-kds.css',(_req,res)=>send(res,'restaurant-v2-kds.css','text/css; charset=utf-8'));
 router.get('/app/restaurant-v2-realtime.js',(_req,res)=>send(res,'vantix-tenant-realtime.js','application/javascript; charset=utf-8'));
 
