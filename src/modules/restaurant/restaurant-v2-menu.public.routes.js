7e11d50b5d8732edb27a6c7266b467d29f7efd8b
juansfer858 <146719055+juansfer858@users.noreply.github.com>
2026-09-09 22:12:03 -0500

    feat(restaurante): crear superficie V2 de Carta

---
+++ src/modules/restaurant/restaurant-v2-menu.public.routes.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const path = require('node:path');
+
+const router = express.Router();
+const webRoot = path.join(__dirname, '../../web');
+const HEADER_VALUE = 'restaurant-v2-menu-v1';
+
+function send(res, file, type) {
+  res.set('Cache-Control', 'no-store, max-age=0');
+  res.set('X-VantixGC-Restaurant-V2-Menu', HEADER_VALUE);
+  if (type) res.type(type);
+  return res.sendFile(path.join(webRoot, file));
+}
+
+router.get('/app/restaurante-v2/carta', (_req, res) => {
+  return send(res, 'restaurant-v2-menu.html', 'html');
+});
+
+router.get('/app/restaurant-v2-menu.css', (_req, res) => {
+  return send(res, 'restaurant-v2-menu.css', 'text/css; charset=utf-8');
+});
+
+router.get('/app/restaurant-v2-menu.js', (_req, res) => {
+  return send(res, 'restaurant-v2-menu.js', 'application/javascript; charset=utf-8');
+});
+
+module.exports = {
+  HEADER_VALUE,
+  restaurantV2MenuPublicRouter: router
+};
