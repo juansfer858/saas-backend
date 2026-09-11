a3030286afcf6dd88178bf77dd5ee5a2d1d8f268
juansfer858 <146719055+juansfer858@users.noreply.github.com>
2026-09-10 22:39:26 -0500

    feat(restaurante): cargar editor promo V28 en Carta

--- src/modules/restaurant/restaurant-v2-menu.public.routes.js
+++ src/modules/restaurant/restaurant-v2-menu.public.routes.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('node:fs');
 const express = require('express');
 const path = require('node:path');
 
@@ -13,12 +14,13 @@ function send(res, file, type) {
   if (type) res.type(type);
   return res.sendFile(path.join(webRoot, file));
 }
-
-router.get('/app/restaurante-v2/carta', (_req, res) => send(res, 'restaurant-v2-menu.html', 'html'));
-router.get('/app/restaurant-v2-menu.css', (_req, res) => send(res, 'restaurant-v2-menu.css', 'text/css; charset=utf-8'));
-router.get('/app/restaurant-v2-menu.js', (_req, res) => send(res, 'restaurant-v2-menu.js', 'application/javascript; charset=utf-8'));
-router.get('/app/restaurant-v2-menu-category-selector-v14.js', (_req, res) => send(res, 'restaurant-v2-menu-category-selector-v14.js', 'application/javascript; charset=utf-8'));
-router.get('/app/restaurant-v2-menu-edit-v27.js', (_req, res) => { res.set('X-VantixGC-Restaurant-Carta-Edit','v27'); return send(res,'restaurant-v2-menu-edit-v27.js','application/javascript; charset=utf-8'); });
-router.get('/app/restaurant-promo-image-editor-v28.js', (_req, res) => { res.set('X-VantixGC-Restaurant-Promo','image-popup-v28'); return send(res,'restaurant-promo-image-editor-v28.js','application/javascript; charset=utf-8'); });
-
+router.get('/app/restaurante-v2/carta', (_req,res)=>{
+  const html=fs.readFileSync(path.join(webRoot,'restaurant-v2-menu.html'),'utf8').replace('</body>','<script src="/app/restaurant-promo-image-editor-v28.js?v=v28"></script></body>');
+  res.set('Cache-Control','no-store, max-age=0');res.set('X-VantixGC-Restaurant-V2-Menu',HEADER_VALUE);res.type('html').send(html);
+});
+router.get('/app/restaurant-v2-menu.css', (_req,res)=>send(res,'restaurant-v2-menu.css','text/css; charset=utf-8'));
+router.get('/app/restaurant-v2-menu.js', (_req,res)=>send(res,'restaurant-v2-menu.js','application/javascript; charset=utf-8'));
+router.get('/app/restaurant-v2-menu-category-selector-v14.js', (_req,res)=>send(res,'restaurant-v2-menu-category-selector-v14.js','application/javascript; charset=utf-8'));
+router.get('/app/restaurant-v2-menu-edit-v27.js', (_req,res)=>{res.set('X-VantixGC-Restaurant-Carta-Edit','v27');return send(res,'restaurant-v2-menu-edit-v27.js','application/javascript; charset=utf-8');});
+router.get('/app/restaurant-promo-image-editor-v28.js', (_req,res)=>{res.set('X-VantixGC-Restaurant-Promo','image-popup-v28');return send(res,'restaurant-promo-image-editor-v28.js','application/javascript; charset=utf-8');});
 module.exports={HEADER_VALUE,restaurantV2MenuPublicRouter:router};
