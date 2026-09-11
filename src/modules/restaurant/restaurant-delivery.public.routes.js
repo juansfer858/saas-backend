196951b3321a1f086d37cbf30da78bef0e1d1e22
juansfer858 <146719055+juansfer858@users.noreply.github.com>
2026-09-11 13:13:24 -0500

    fix(restaurante): cargar envío inmediato Domicilios V86

--- src/modules/restaurant/restaurant-delivery.public.routes.js
+++ src/modules/restaurant/restaurant-delivery.public.routes.js
@@ -7,16 +7,19 @@ const express = require('express');
 const router = express.Router();
 const deliveryUi = path.join(__dirname, '../../web/restaurant-delivery-ui.js');
 const deliveryCustomerAutofill = path.join(__dirname, '../../web/restaurant-delivery-customer-autofill-v83.js');
+const deliverySendProduction = path.join(__dirname, '../../web/restaurant-delivery-send-production-v86.js');
 
 router.get('/app/restaurant-delivery-ui.js', async (_req, res, next) => {
   try {
-    const [baseUi, customerAutofill] = await Promise.all([
+    const [baseUi, customerAutofill, sendProduction] = await Promise.all([
       fs.promises.readFile(deliveryUi, 'utf8'),
-      fs.promises.readFile(deliveryCustomerAutofill, 'utf8')
+      fs.promises.readFile(deliveryCustomerAutofill, 'utf8'),
+      fs.promises.readFile(deliverySendProduction, 'utf8')
     ]);
     res.set('Cache-Control', 'no-store');
     res.set('X-VantixGC-Restaurant-Delivery-Customer-Autofill', 'v83');
-    res.type('application/javascript').send(`${baseUi}\n\n${customerAutofill}\n`);
+    res.set('X-VantixGC-Restaurant-Delivery-Send-Production', 'v86');
+    res.type('application/javascript').send(`${baseUi}\n\n${customerAutofill}\n\n${sendProduction}\n`);
   } catch (error) {
     next(error);
   }
